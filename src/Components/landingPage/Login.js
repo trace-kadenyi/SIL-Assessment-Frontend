e065eff1db945ee77aaf9391c8dd23ca7541cdf9
Tracey Kadenyi <treykadenyi@gmail.com>
2024-02-15 22:40:06 +0300

    Remove Home route

--- src/Components/landingPage/Login.js
+++ src/Components/landingPage/Login.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { auth, provider } from "./firebaseconfig.js";
 import { signInWithPopup } from "firebase/auth";
-
-import Home from "../home/Homepage.js";
 import LogoutFunc from "./LogoutFunc.js";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [user, setUser] = useState("");
+  const navigate = useNavigate();
 
   const handleClick = () => {
     signInWithPopup(auth, provider).then((data) => {
@@ -26,7 +26,15 @@ const Login = () => {
             className="text-white mt-5 text-center mx-auto opacity-70 p-4 uppercase font-bold tracking-wide overline text-white mt-10 text-center w-100 md:w-70 lg:w-3/4
         "
           >
-            You are logged in as {user}. <Home />
+            {/* navigate to /home */}
+            Welcome {user}! Click the button below to go to the home page.
+            <button
+              onClick={() => navigate("/home")}
+              type="button"
+              className="py-2 px-4 max-w-md flex justify-center items-center bg-sky-50 font-bold hover:bg-black focus:ring-red-500 focus:ring-offset-red-200 text-black w-40 hover:text-white transition ease-in duration-200 text-center text-base font-bold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg display-flex justify-center items-center mt-10 mx-auto"
+            >
+              HOME
+            </button>
           </h5>
           <LogoutFunc />
         </div>
@@ -34,14 +42,14 @@ const Login = () => {
         <button
           onClick={handleClick}
           type="button"
-          class="py-2 px-4 max-w-md flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg
+          className="py-2 px-4 max-w-md flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg
           display-flex justify-center items-center mt-10 mx-auto mb-10"
         >
           <svg
             width="20"
             height="20"
             fill="currentColor"
-            class="mr-2"
+            className="mr-2"
             viewBox="0 0 1792 1792"
             xmlns="http://www.w3.org/2000/svg"
           >
