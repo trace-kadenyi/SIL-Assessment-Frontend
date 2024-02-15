c048dd9a8eee18dacf3112ebb205ea0db80203b1
Tracey Kadenyi <treykadenyi@gmail.com>
2024-02-15 09:18:21 +0300

    use environment variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+import React from "react";
+import Login from "./Components/landingPage/Login";
+import LandingPage from "./Components/landingPage/LandingPage";
+
 const App = () => {
   return (
-    <div
-      className="bg-slate-800 flex h-screen flex-col"
-    >
-      <h1
-        className="text-white font-bold underline justify-center flex text-4xl mt-20 text-center opacity-70 tracking-wide"
-      >
-        Hello World
-      </h1>
-      <p
-        className="text-white text-2xl mt-20 text-center w-100 md:w-70 lg:w-3/4 mx-auto opacity-70 p-4 
-        "
-      >
-        The current application allows you to log in using your Google account.
-        Once logged in, you can view a list of all the users and some of their
-        details such as their names and the number of albums they have. You can
-        also view the albums of each user and the photos in each album.
-        Additionally, you can edit the title of an album.
-      </p>
+    <div className="App">
+      {/* landing page */}
+      <LandingPage />
     </div>
   );
 };
--- src/Components/landingPage/firebaseconfig.js
+++ src/Components/landingPage/firebaseconfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBewyX5UxBVt5tUMN9v20jBDc0C7sAqe2M",
-  authDomain: "sil-assessment-authentication.firebaseapp.com",
-  projectId: "sil-assessment-authentication",
-  storageBucket: "sil-assessment-authentication.appspot.com",
-  messagingSenderId: "441034352262",
-  appId: "1:441034352262:web:1112649768c647e436d307",
-  measurementId: "G-HB4YR4GG7T"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
