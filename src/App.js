a4bf1530e9109af0c45ec7df85195ecc82f32c66
Tracey Kadenyi <treykadenyi@gmail.com>
2024-02-15 22:39:14 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import Login from "./Components/landingPage/Login";
+import { Routes, Route } from "react-router-dom";
+
 import LandingPage from "./Components/landingPage/LandingPage";
+import Home from "./Components/home/Homepage";
+import User from "./Components/user/User";
 
 const App = () => {
   return (
-    <div className="App">
-      {/* landing page */}
-      <LandingPage />
+    <div
+      className="bg-sky-800 w-full h-full min-h-screen
+    "
+    >
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/user/:id" element={<User />} />
+      </Routes>
     </div>
   );
 };
