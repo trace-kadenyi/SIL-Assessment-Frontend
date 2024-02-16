c66791f827422eb961f243751a970b61d1216429
Tracey Kadenyi <treykadenyi@gmail.com>
2024-02-16 17:28:32 +0300

    Optimize function that fetches albums

--- src/Components/album/Album.js
+++ src/Components/album/Album.js
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Album = ({ albums, photos, getAlbumPhotos }) => {
+  const [getAlbumClicked, setGetAlbumClicked] = useState(false);
   //    get album id
   const { id } = useParams();
   //   find photos for the album
   const album = albums.find((album) => album.id === parseInt(id));
-  // get album photos
-  getAlbumPhotos(album.id);
+
+  //  get album photos
+  const handleGetAlbumPhotos = () => {
+    getAlbumPhotos(album.id);
+    setGetAlbumClicked(true);
+  };
+
+  //  navigate to photo page
+  const navigate = useNavigate();
+
+  //  handle photo click
+  const handlePhotoClick = (photo) => {
+    navigate(`/photo/${photo.id}`);
+  };
 
   return (
     <div className="p-10 lg:p-20 text-white">
       <h2 className="text-4xl font-extrabold text-white underline mb-10">
         {album.title}
       </h2>
+      <button
+        onClick={handleGetAlbumPhotos}
+        className="bg-sky-500 text-white p-2 rounded-md mb-5"
+      >
+        Get Album Photos
+      </button>
       {/* list the album photos */}
-      {photos.length > 0 ? (
+      {photos.length > 0 && getAlbumClicked ? (
         <div>
           <h3 className="font-bold text-white mb-5 italic overline">
             Album No. {album.id} Photos
@@ -31,13 +50,16 @@ const Album = ({ albums, photos, getAlbumPhotos }) => {
                   src={photo.url}
                   alt={photo.title}
                   className="w-16 lg:w-20"
+                  onClick={() => handlePhotoClick(photo)}
                 />
               </li>
             ))}
           </ul>
         </div>
       ) : (
-        <p className="text-white">Loading...</p>
+        <p className="text-white">
+          Click the button above to view the Album Photos
+        </p>
       )}
     </div>
   );
