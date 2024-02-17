a5e54e17cd895d23a0e4abe25083d7a8e9c030d5
Tracey Kadenyi <treykadenyi@gmail.com>
2024-02-17 15:53:28 +0300

    Fix bug on fetching user albums

--- src/Components/user/User.js
+++ src/Components/user/User.js
@@ -1,35 +1,26 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-const User = ({
-  users,
-  userAlbums,
-
-}) => {
-  // get user information
+const User = ({ users, userAlbums }) => {
+  // get the user id from the URL
   const { id } = useParams();
+  // find the user by id
   const user = users.find((user) => user.id === parseInt(id));
+  // get the user's albums
+  const albums = userAlbums[id] || [];
 
   return (
     <div className="p-10 lg:p-20 text-white">
       <h2 className="text-4xl font-extrabold text-white underline mb-10">
+        {/* get the user's name */}
         {user.name}
       </h2>
       {/* list the user's albums */}
-      {userAlbums.length > 0 ? (
-        <div>
-          <h3 className="font-bold text-white mb-5 italic overline">Albums</h3>
-          <ol style={{ listStyleType: "circle" }}>
-            {userAlbums.map((album) => (
-              <li key={album.id} className="text-white">
-                {album.title}
-              </li>
-            ))}
-          </ol>
-        </div>
-      ) : (
-        <p className="text-white">Loading...</p>
-      )}
+      <ol style={{ listStyle: "circle" }}>
+        {albums.map((album, index) => (
+          <li key={index}>{album.title}</li>
+        ))}
+      </ol>
     </div>
   );
 };
